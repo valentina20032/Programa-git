f7fd79d93a61f66a2d8e42f5032f0169a31ffb7e
valentina golu martinez <valenly26@gmail.com>
2025-03-06 03:00:13 -0500

    primer commit

--- sena interfaz.html/php/banner.php
+++ sena interfaz.html/php/banner.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>document</title>
 </head>
 <body>
  <div class="banner">
     <div class="container">
         <div class="row">
             <div class="-sm-6">
-             <img src="img/ingvecon_logo.png" alt="responsive logo" class="rounded">   
+             <img src="img/SPANIX.png" alt="responsive logo" class="rounded">   
             </div>
     
     
--- sena interfaz.html/php/formulario.php
+++ sena interfaz.html/php/formulario.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div class="crad text-center">
-        bienvenido
+        SpaNix Reservas
     </div>
     <div class="row pt-5 justify-content-center">
         <!--inicio del formulario-->
@@ -16,7 +16,7 @@
         <div class="card-header">
         <input type="text" name= "tipo_operacion" value="guardar" hidden="true">
         <div>
-            <h5>formulario para el registro de pacientes</h5>
+            <h5>Reserva tu cita</h5>
         </div>
         <div class="form-group">
             <select name="tipo_doc" id="tipo_doc" class= "form-control">
@@ -29,15 +29,33 @@
                 </select>
         </div>
         <div class="form-group">
-            <input type="date" name="fecha" id="fecha" name="documento" class="form-control" placeholder="digite su numero de identidad">
-    
+            <input type="text" name="documento" id="documento" name="documento" class="form-control" placeholder="digite su numero de identidad">
     </div>
     <div class="form-group">
-            <input type="text" name="documento" id="fecha" class="form-control">
+            <input type="text" name="documento" id="fecha" class="form-control" placeholder="digite numero telefonico">
+</div>
             <div class="form-group">
-            <input type="text" name="nombre" id="nombre" name="documento" class="form-control" placeholder="digite sus nombres">
-    
+            <input type="date" name="fecha" id="fecha" class="form-control">
     </div>
+    <div class="form-group">
+        <input type="text" name="nombre" id="nombre" class="form-control" placeholder="digite su nombre">
     </div>
+     <div class="form-group">
+        <input type="text" name="apellidos" id="apellidos" class="form-control"placeholder="digite su apellido">
+</div>
+<div class="form-group">
+            <select name="sexo" id="sexo" class= "form-control">
+                <option value="">selecione el sexo</option>
+                <option value="masculino">masculino</option>
+                <option value="femenino">femenino</option>
+                </select>
+        </div>
+</div class="form-group">
+<button type="submit" class="btn.info btn-block">siguiente</button>
+</div>
+</div>
+<a href="" class="mav-link"href="index.php"><--index</a>
+</form>
+</div>
 </body>
 </html>
